fc874dd131eed3dfb77c0368b72f18873b024393
Alex <nguyenngocgiahuy1110@gmail.com>
2022-12-12 20:01:41 +0700

    Push Project

---
+++ QLCuaHang/QLCuaHang/DataAccess/ConnectionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    internal class ConnectionInfo
+    {
+        public static string CONNECTION_STRING = @"Data Source=DESKTOP-JMK2MMF\SQLEXPRESS;Initial Catalog = ShopManager; Integrated Security = True";
+    }
+}
